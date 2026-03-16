092e96c58c762d60faad17bce3c05a1bf47dde3a
stanislav-t <stanislav.topikha@gmail.com>
2026-03-16 12:40:34 -0400

    Binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,21 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+  let lo = 0,
+      hi = haystack.length;
 
+  do {
+    const mid = Math.floor(lo + (hi - lo) / 2);
+    const val = haystack[mid];
+    if (val === needle) {
+      return true;
+    }
+
+    if (val > needle) {
+      hi = mid;
+      continue;
+    }
+
+    lo = mid + 1;g
+  } while(lo < hi)
+
+  return false;
 }
