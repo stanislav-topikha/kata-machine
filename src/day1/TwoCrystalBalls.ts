a8e004628abe05904feb6a7a0ccf2c6efe305f8d
stanislav-t <stanislav.topikha@gmail.com>
2026-03-16 13:32:33 -0400

    Two glass balls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,20 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+  const step = Math.floor(breaks.length ** 0.5);
 
+  for (
+    let i = step;
+    i < breaks.length;
+    i += step
+  ) {
+    if (breaks[i]) {
+      i -= step;
+      while(!breaks[i]) {
+        i++;
+      }
+
+      return i;
+    }
+  }
+
+  return -1;
 }
