ff69e90cb491716a472f838e169356b007435d28
stanislav-t <stanislav.topikha@gmail.com>
2026-03-19 11:19:40 -0400

    Queue

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,48 @@
+type QNode<T> = {
+  value: T,
+  next?: QNode<T>,
+}
+
 export default class Queue<T> {
     public length: number;
-
-    
+    private head?: QNode<T>;
+    private tail?: QNode<T>;
 
     constructor() {
+      this.head = this.tail = undefined;
+      this.length = 0;
     }
 
-    enqueue(item: T): void {
+    enqueue(item: T): void { // push
+      this.length++;
 
-}
-    deque(): T | undefined {
+      const newNode = {value: item} as QNode<T>;
 
-}
+      if (!this.tail) {
+        this.tail = this.head = newNode;
+        return;
+      }
+
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    deque(): T | undefined { // pop
+      if (!this.head) {
+        return;
+      }
+
+      this.length--;
+
+      const head = this.head;
+      this.head = this.head.next;
+
+      if (!this.head) {
+        this.tail = undefined;
+      }
+
+      return head.value;
+    }
     peek(): T | undefined {
-
-}
+      return this.head?.value;
+    }
 }
