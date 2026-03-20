7a832da9f02aacf668bd1741789e5f4308178f59
stanislav-t <stanislav.topikha@gmail.com>
2026-03-20 18:09:47 -0400

    Stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,45 @@
+type Node<T> = {
+  value: T
+  prev?: Node<T>,
+}
+
 export default class Stack<T> {
     public length: number;
+    private head?: Node<T>;
 
-    
 
     constructor() {
+      this.head = undefined;
+      this.length = 0;
     }
 
     push(item: T): void {
+      this.length++;
+      const node = {value: item} as Node<T>;
 
-}
+      if (!this.head) {
+        this.head = node;
+        return;
+      }
+
+      node.prev = this.head;
+      this.head = node;
+    }
+
     pop(): T | undefined {
+      if (!this.head) {
+        return;
+      }
 
-}
+      this.length--;
+
+      const popItem = this.head;
+      this.head = this.head.prev;
+
+      return popItem.value;
+    }
+
     peek(): T | undefined {
-
-}
+      return this.head?.value;
+    }
 }
