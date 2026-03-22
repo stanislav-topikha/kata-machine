9f41445d399bc4dbd470df105d9d9ebafdac1f2a
stanislav-t <stanislav.topikha@gmail.com>
2026-03-22 12:40:09 -0400

    Maze

--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -1,3 +1,53 @@
-export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
+const dirs = [
+  [-1, 0],
+  [1, 0],
+  [0, -1],
+  [0, 1],
+];
 
+function walk(maze: string[], wall: string, curr: Point, end: Point, seen: boolean[][], path: Point[]):boolean {
+  //base cases
+  if (curr.x < 0 || curr.x >= maze[0].length || curr.y < 0 || curr.y >= maze.length) {
+    return false;
+  }
+
+  if (maze[curr.y][curr.x]===wall) {
+    return false;
+  }
+
+  if (curr.x === end.x && curr.y === end.y) {
+    path.push(end);
+    return true;
+  }
+
+  if (seen[curr.x]?.[curr.y]) {
+    return false;
+  }
+
+  //recurse:
+  //pre
+   path.push(curr);
+   seen[curr.x]??=[];
+   seen[curr.x][curr.y] = true;
+  //recurse
+  for (const [x, y] of dirs) {
+    if (walk(maze, wall, {
+      x: curr.x + x,
+      y: curr.y + y
+    }, end, seen, path)) {
+      return true;
+    }
+  }
+  //post
+   path.pop();
+   return false;
+}
+
+export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
+  const seen: boolean[][] = [];
+  const path: Point[] = [];
+
+  walk(maze, wall, start, end, seen, path);
+
+  return path;
 }
