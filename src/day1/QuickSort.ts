26dbde39fbdf3848a7a66f3a42b6d7de3e8ef60d
stanislav-t <stanislav.topikha@gmail.com>
2026-03-23 11:13:34 -0400

    Quick sort

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,32 @@
-export default function quick_sort(arr: number[]): void {
+function partition(arr: number[], lo: number, hi: number):number {
+  const pivot = arr[hi];
 
+  let idx = lo - 1;
+
+  for (let i = lo; i < hi; i++) {
+    if (arr[i] <= pivot) {
+      idx++;
+      [arr[i], arr[idx]] = [arr[idx], arr[i]]
+    }
+  }
+
+  idx++;
+  [arr[hi], arr[idx]] = [arr[idx], arr[hi]];
+
+  return idx;
+}
+
+function qs(arr: number[], lo: number, hi: number) {
+  if (lo>= hi) {
+    return;
+  }
+
+  const pivotIndex = partition(arr, lo, hi);
+
+  qs(arr, lo, pivotIndex - 1);
+  qs(arr, pivotIndex + 1, hi);
+}
+
+export default function quick_sort(arr: number[]): void {
+  qs(arr, 0, arr.length - 1);
 }
