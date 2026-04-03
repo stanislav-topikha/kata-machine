3cfd2944f24304ae4196a223ada7b703a8447287
stanislav-t <stanislav.topikha@gmail.com>
2026-04-03 15:30:22 -0400

    BT pre

--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,3 +1,17 @@
-export default function pre_order_search(head: BinaryNode<number>): number[] {
+function walk (curr: BinaryNode<number> | null, path: number[]):number[] {
+  //base case
+  if (!curr) {
+    return path;
+  }
+  //pre
+  path.push(curr.value);
+  //recursion
+  walk(curr.left, path);
+  walk(curr.right, path);
+  //post
+  return path;
+}
 
+export default function pre_order_search(head: BinaryNode<number>): number[] {
+  return walk(head, []);
 }
