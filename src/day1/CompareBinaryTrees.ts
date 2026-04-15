2f77792bb07f2624deb9df0f54d314cf8111a9e6
stanislav-t <stanislav.topikha@gmail.com>
2026-04-15 14:06:04 -0400

    CompareBinaryTrees

--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,15 @@
 export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+  if (a === b) {
+    return true;
+  }
 
+  if (a === null || b === null) {
+    return false;
+  }
+
+  if (a.value !== b.value) {
+    return false;
+  }
+
+  return compare(a.left, a.left) && compare(a.right, b.right);
 }
