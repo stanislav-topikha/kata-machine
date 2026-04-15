2d81df41bb8a92a78644dcbd7fbd00776e3e218b
stanislav-t <stanislav.topikha@gmail.com>
2026-04-15 11:10:56 -0400

    BTBFS v1

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,26 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+  let q = [head];
 
+  while(q.length) {
+    let tmpQ = [];
+
+    for (const node of q) {
+      if (node.value === needle) {
+        return true;
+      }
+
+      if (node.left) {
+        tmpQ.push(node.left);
+      }
+
+      if (node.right) {
+        tmpQ.push(node.right);
+      }
+    }
+
+    q = tmpQ;
+    tmpQ = [];
+  }
+
+  return false;
 }
