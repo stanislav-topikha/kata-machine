d38d032b68abc97e6c3bb98d0e08c3bc2b12b923
stanislav-t <stanislav.topikha@gmail.com>
2026-04-16 23:51:29 -0400

    DFSOnBST

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,19 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+function search(node: BinaryNode<number> | null, needle: number): boolean {
+  if (!node) {
+    return false;
+  }
 
+  if (node.value === needle) {
+    return true;
+  }
+
+  if (needle < node.value) {
+    return search(node.left, needle);
+  }
+
+  return search(node.right, needle);
+}
+
+export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+  return search(head, needle);
 }
