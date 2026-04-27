91e126701ba0c125197b787015f8d5151a99ed46
stanislav-t <stanislav.topikha@gmail.com>
2026-04-26 21:43:20 -0400

    DFSGraphList

--- src/day1/DFSGraphList.ts
+++ src/day1/DFSGraphList.ts
@@ -1,3 +1,39 @@
-export default function dfs(graph: WeightedAdjacencyList, source: number, needle: number): number[] | null {
+function walk(graph: WeightedAdjacencyList, curr: number, needle: number, seen: boolean[], path: number[]):boolean {
+  if (curr === needle) {
+    path.push(curr);
+    return true;
+  }
 
+  if (seen[curr]) {
+    return false;
+  }
+
+  //pre
+  path.push(curr);
+  seen[curr] = true;
+  //recurse
+  const list = graph[curr];
+
+  for (const edge of list) {
+    if (walk(graph, edge.to, needle, seen, path)) {
+      return true;
+    }
+  }
+  //post
+  path.pop();
+  return false;
+}
+
+export default function dfs(graph: WeightedAdjacencyList, source: number, needle: number): number[] | null {
+  const seen = new Array(graph.length).fill(false);
+  const path: number[] = [];
+
+  walk(graph, source, needle, seen, path);
+
+  if (path.length === 0) {
+    return null;
+  }
+
+
+  return path;
 }
